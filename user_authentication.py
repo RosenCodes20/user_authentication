9a7adbdfa86325dcf0ffc9074cc434a673eed94d
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-11-02 15:29:26 +0200

    Updated user_authentication.py
    Added a new logout operation to the user authentication.

--- user_authentication.py
+++ user_authentication.py
@@ -16,7 +16,7 @@ def login():
     username = input("Enter your username:")
     password = input("Enter your password:")
     if username in user_credentials and user_credentials[username] == {"Password": password}:
-        print("You've successfully loged in!")
+        print("You've successfully logged in!")
     else:
         print("Login failed.Invalid username or password.")
 
@@ -30,12 +30,21 @@ def check_profile():
             print(f"Password: {details['Password']}")
 
 
+def logout():
+    if user_credentials:
+        user_credentials.clear()
+        print("You've successfully logged out!")
+    else:
+        print("Please first go and make registration and log in!")
+
+
 while True:
     print("\n Welcome to the python console login page!")
     print("1. Registration")
     print("2. Login")
     print("3. Check profile")
-    print("4. Exit")
+    print("4. Logout")
+    print("5. Exit")
 
     choice = int(input("Enter a operation(1, 2, 3, 4):"))
 
@@ -46,6 +55,8 @@ while True:
     elif choice == 3:
         check_profile()
     elif choice == 4:
+        logout()
+    elif choice == 5:
         print("Goodbye!!")
         break
     else:
